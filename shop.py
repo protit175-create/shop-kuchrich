39aab9279a668a68621e2ccbc38c43d019d8b04f
protit175-create <protit175@gmail.com>
2026-07-23 08:43:38 +0700

    Add files via upload

--- shop.py
+++ shop.py
@@ -443,6 +443,8 @@ def home():
         """
     html_content = f"<!DOCTYPE html><html><head><meta name='google-site-verification' content='tTmA8Wj8zOGGSbDXkTtrk0KHC1VabAkyAqxpRAnoOWY' /><title>Kuchrich - Shop Game Roblox</title>{BASE_CSS}</head><body>{get_header()}<div class='container'><h2>DANH MỤC DỊCH VỤ</h2><div class='card-grid'>{cards_html}</div></div></body></html>"
     return html_content
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
         username = request.form.get("username", "").strip()
